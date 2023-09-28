3c6acbd0f531b21d9c4e2a8b6fcdfef3decada16
Lara <Laptop@LARA>
2023-09-28 23:24:55 +0200

    Guide model added, basics implemented

---
+++ Touristic App/Models/GuideContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Touristic_App.Models
+{
+    public class GuideContext : DbContext
+    {
+        public GuideContext(DbContextOptions<GuideContext> options) : base(options) { }
+
+        public DbSet<Guide> Guides { get; set; } = null!;
+
+    }
+
+}
--- Touristic App/Program.cs
+++ Touristic App/Program.cs
@@ -1,13 +1,15 @@
-using Microsoft.EntityFrameworkCore;                                 //I added this
+using Microsoft.EntityFrameworkCore;                                                         //I added this
 using TodoApi.Models;
+using Touristic_App.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<TodoContext>(opt =>                    //I added this
+builder.Services.AddDbContext<TodoContext>(opt =>                                            //I added this
     opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDbContext<GuideContext>(opt => opt.UseInMemoryDatabase("GuidesList"));   //I added this
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,7 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseAuthorization();  //Ovaj middleware dodaje podršku za autentikaciju i autorizaciju, ali u ovom kodu ne postoje konkretni pravila za autorizaciju, pa æe svi zahtjevi biti odobreni.
 
 app.MapControllers();
 
