57cb0bac66de076a03b81506b90fac4bf6a55368
Lara <Laptop@LARA>
2023-09-28 13:25:29 +0200

    Change of the project type

---
+++ TouristicGuide/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TouristicGuide.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public IConfiguration _config { get; set; }
+        public AppDbContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+        }
+
+        public DbSet<Guide> Guides { get; set; }
+    }
+}
---
+++ TouristicGuide/Data/Guide.cs
@@ -0,0 +1,13 @@
+﻿namespace TouristicGuide.Data
+{
+    public class Guide
+    {
+        public String Username;
+        public String FirstName;
+        public String LastName;
+        public String Email;
+        public DateTime Birthday;
+        public String Phone;
+        public String Languages;
+    }
+}
