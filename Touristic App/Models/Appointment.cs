3d082ab8754a9a2265f20d6b26eeae86ac748086
lara-petkovic <larapetkovic021@gmail.com>
2023-10-03 22:57:19 +0200

    Touristic  App project removed

--- Touristic App/Models/Appointment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Touristic_App.Models
-{
-    public class Appointment
-    {
-        public int Id { get; set; }
-        public int TourId { get; set; }
-        public DateTime DateTime { get; set; }
-    }
-}
